83104b51c934e9c343f1b6b997ef2908c2964af2
mrmade6 <mrmade6@gmail.com>
2025-06-01 16:08:21 +0300

    pravki

--- src/main/java/com/shxtnyra/forum/dto/post/PostCreateDTO.java
+++ src/main/java/com/shxtnyra/forum/dto/post/PostCreateDTO.java
@@ -1,36 +1,13 @@
 package com.shxtnyra.forum.dto.post;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
-import lombok.Data;
+import java.util.List;
 
-import java.util.Set;
-
-@Data
 @Builder
-public class PostCreateDTO {
-    @NotBlank
-    @Size(max = 200)
-    private final String title;
-
-    @NotBlank
-    private final String content;
-
-    @Size(max = 500)
-    private final String previewContent;
-
-    @NotNull
-    private final Long topicId;
-
-    private final String coverImageUrl;
-
-    private final Set<String> tags;
-
-    @Size(max = 200)
-    private final String metaDescription;
-
-    @Size(max = 200)
-    private final String metaKeywords;
-}
+public record PostCreateDTO(
+    @NotBlank String title,
+    @NotNull @Size(min = 1) List<ContentBlockDTO> blocks,
+    @NotNull Long topicId,
+    boolean draft
+) {}
