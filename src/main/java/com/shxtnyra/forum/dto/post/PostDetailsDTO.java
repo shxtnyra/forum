d204f73f6b02c7e875b9e9766a4f00373d3c02b0
mrmade6 <mrmade6@gmail.com>
2025-06-01 16:08:46 +0300

    pravki

--- src/main/java/com/shxtnyra/forum/dto/post/PostDetailsDTO.java
+++ src/main/java/com/shxtnyra/forum/dto/post/PostDetailsDTO.java
@@ -1,34 +1,21 @@
 package com.shxtnyra.forum.dto.post;
 
-import com.shxtnyra.forum.dto.tag.TagDTO;
-import com.shxtnyra.forum.dto.user.UserShortDTO;
-import com.shxtnyra.forum.enums.PostStatus;
 import lombok.Builder;
-import lombok.Data;
-
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.util.List;
 
-@Data
 @Builder
-public class PostDetailsDTO {
-    private final Long id;
-    private final String title;
-    private final String content;
-    private final String previewContent;
-    private final LocalDateTime createAt;
-    private final LocalDateTime updateAt;
-    private final LocalDateTime publishedAt;
-    private final UserShortDTO author;
-    private final PostStatus status;
-    private final String coverImageUrl;
-    private final Set<TagDTO> tags;
-    private final boolean isPinned;
-    private final boolean isPromoted;
-    private final boolean isEditorChoice;
-    private final String metaDescription;
-    private final String metaKeywords;
-    private final int viewCount;
-    private final int likeCount;
-    private final int dislikeCount;
-}
+public record PostDetailsDTO(
+    Long id,
+    String title,
+    String customUri,
+    List<ContentBlockDTO> blocks,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
+    UserShortDTO author,
+    int viewCount,
+    int likeCount,
+    int dislikeCount,
+    boolean draft
+) {}
+
