0c2aa11dfe3b472fcac213485a63962efd8d2c5f
mrmade6 <mrmade6@gmail.com>
2025-06-01 16:09:12 +0300

    pravki

--- src/main/java/com/shxtnyra/forum/dto/post/PostShortDTO.java
+++ src/main/java/com/shxtnyra/forum/dto/post/PostShortDTO.java
@@ -1,21 +1,15 @@
 package com.shxtnyra.forum.dto.post;
 
-import com.shxtnyra.forum.dto.user.UserShortDTO;
 import lombok.Builder;
-import lombok.Data;
-
 import java.time.LocalDateTime;
 
-@Data
 @Builder
-public class PostShortDTO {
-    private final Long id;
-    private final String title;
-    private final LocalDateTime createAt;
-    private final UserShortDTO author;
-    private final String previewContent;
-    private final String coverImageUrl;
-    private final int viewCount;
-    private final int likeCount;
-    private final int dislikeCount;
-}
+public record PostShortDTO(
+    Long id,
+    String title,
+    String customUri,
+    LocalDateTime createdAt,
+    UserShortDTO author,
+    int viewCount,
+    int likeCount
+) {}
